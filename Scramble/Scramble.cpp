631b4e6f09e1fcbfd85640777d1db2abac20c006
Nicolas Perez-merino <nperezme@mail.umw.edu>
2025-05-25 14:30:02 -0400

    Added functionality to load and store words from dictionary file.

--- Scramble/Scramble.cpp
+++ Scramble/Scramble.cpp
@@ -1,4 +1,7 @@
 #include "Scramble.h"
+#include <fstream>
+
+using std::ifstream;
 
 // Implementation for Scramble game logic
 
@@ -15,7 +18,17 @@ void Scramble::game_intro() {
 }
 
 bool Scramble::load_words() {
-	return false;
+	ifstream words("dictionary.txt");
+	string word;
+
+	if (!words.is_open()) {
+		return false;
+	}
+
+	while (getline (words, word)) {
+		insert_word(word);
+	}
+	return true;
 }
 
 bool Scramble::start_game() {
@@ -28,4 +41,18 @@ string Scramble::scramble_word(string word) {
 
 void Scramble::end_game() {
 
+}
+
+void Scramble::insert_word(string word) {
+	if (word.length() <= Small) {
+		smallWords.push_back(word);
+		return;
+	}
+	if (word.length() > Small && word.length() <= Medium) {
+		mediumWords.push_back(word);
+		return;
+	}
+	if (word.length() >= Large){
+		largeWords.push_back(word);
+	}
 }
--- Scramble/Scramble.h
+++ Scramble/Scramble.h
@@ -20,6 +20,9 @@ class Scramble {
 		// Load all words from text file and return true if succesful
 		bool load_words();
 
+		// Helper method to choose what word goes into each vector
+		void insert_word(string word);
+
 		// Begin game and return true if player wins
 		bool start_game();
 
@@ -30,12 +33,13 @@ class Scramble {
 		void end_game();
 	private:
 		int numCorrect; // Number of words player got correct
-		enum Word { // Defined constants for word lengths
-			Small = 4,
-			Medium = 6,
-			Large = 8
-		};
-		vector<string> smallWords; // Words with length <= Word.Small
-		vector<string> mediumWords; // Words with length > Word.Small and <= Word.Medium
-		vector<string> largeWords; // Words with length > Word.Medium and <= Word.Large
+		vector<string> smallWords; // Words with length <= Small
+		vector<string> mediumWords; // Words with length > Small and <= Medium
+		vector<string> largeWords; // Words with length > Medium and <= Large
+};
+
+enum Word { // Defined constants for word lengths
+	Small = 4,
+	Medium = 6,
+	Large = 8
 };
